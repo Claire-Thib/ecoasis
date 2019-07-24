678d372447279f2935a6fb027b0acd5d7c420304
Claire <clairethx@gmail.com>
2019-07-24 09:54:56 -0700

    Init commit

---
+++ plastics.js
@@ -0,0 +1,12 @@
+function hide(){
+
+document.getElementById('image').style='display:none';
+document.getElementById('image2').style='display:block';
+
+}
+
+function show(){
+
+document.getElementById('image').style='display:block';
+document.getElementById('image2').style='display:none';
+}
