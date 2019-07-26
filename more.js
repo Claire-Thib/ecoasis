34a9a6889a141032928b7fc2944e7355e8b5155e
Claire <clairethx@gmail.com>
2019-07-26 14:16:38 -0700

    temperature stuff

--- more.js
+++ more.js
@@ -1,95 +1,161 @@
 function but1(){
-  document.getElementById('therm1').style = 'display:block'
-  document.getElementById('therm2').style = 'display:none'
-  document.getElementById('therm3').style = 'display:none'
-  document.getElementById('therm4').style = 'display:none'
-  document.getElementById('therm5').style = 'display:none'
-  document.getElementById('therm6').style = 'display:none'
-  document.getElementById('none').style.display='block'
-  document.getElementById('one').style.display='none'
-  document.getElementById('two').style.display='none'
-  document.getElementById('four').style.display='none'
-  document.getElementById('six').style.display='none'
-  document.getElementById('twelve').style.display='none'
-
+  document.getElementById('therm1').style = 'display:block';
+  document.getElementById('therm2').style = 'display:none';
+  document.getElementById('therm3').style = 'display:none';
+  document.getElementById('therm4').style = 'display:none';
+  document.getElementById('therm5').style = 'display:none';
+  document.getElementById('therm6').style = 'display:none';
+  document.getElementById('none').style.display='block';
+  document.getElementById('one').style.display='none';
+  document.getElementById('two').style.display='none';
+  document.getElementById('four').style.display='none';
+  document.getElementById('six').style.display='none';
+  document.getElementById('twelve').style.display='none';
+  document.getElementById('black1').style.display='block';
+  document.getElementById('simonhappy').style.display='block';
+  document.getElementById('hbeach').style.display='block';
+  document.getElementById('black2').style.display='none';
+  document.getElementById('coral').style.display='none';
+  document.getElementById('black3').style.display='none';
+  document.getElementById('black4').style.display='none';
+  document.getElementById('glacier').style.display='none';
+  document.getElementById('sealevel').style.display='none';
+  document.body.style.backgroundColor= "#FFF" ;
+  document.getElementById('title4').style.color='black';
 
 }
-
 function but2(){
-  document.getElementById('therm1').style = 'display:none'
-  document.getElementById('therm2').style = 'display:block'
-  document.getElementById('therm3').style = 'display:none'
-  document.getElementById('therm4').style = 'display:none'
-  document.getElementById('therm5').style = 'display:none'
-  document.getElementById('therm6').style = 'display:none'
-  document.getElementById('none').style.display='none'
-  document.getElementById('one').style.display='block'
-  document.getElementById('two').style.display='none'
-  document.getElementById('four').style.display='none'
-  document.getElementById('six').style.display='none'
-  document.getElementById('twelve').style.display='none'
+  document.getElementById('therm1').style = 'display:none';
+  document.getElementById('therm2').style = 'display:block';
+  document.getElementById('therm3').style = 'display:none';
+  document.getElementById('therm4').style = 'display:none';
+  document.getElementById('therm5').style = 'display:none';
+  document.getElementById('therm6').style = 'display:none';
+  document.getElementById('none').style.display='none';
+  document.getElementById('one').style.display='block';
+  document.getElementById('two').style.display='none';
+  document.getElementById('four').style.display='none';
+  document.getElementById('six').style.display='none';
+  document.getElementById('twelve').style.display='none';
+  document.getElementById('black1').style.display='none';
+  document.getElementById('simonhappy').style.display='none';
+  document.getElementById('hbeach').style.display='none';
+  document.getElementById('black2').style.display='block';
+  document.getElementById('coral').style.display='block';
+  document.getElementById('black3').style.display='none';
+  document.getElementById('glacier').style.display='none';
+  document.getElementById('black4').style.display='none';
+  document.getElementById('sealevel').style.display='none';
+  document.body.style.backgroundColor= "#DFDFDF" ;
+  document.getElementById('title4').style.color='black';
 
 }
 
 function but3(){
-  document.getElementById('therm1').style = 'display:none'
-  document.getElementById('therm2').style = 'display:none'
-  document.getElementById('therm3').style = 'display:block'
-  document.getElementById('therm4').style = 'display:none'
-  document.getElementById('therm5').style = 'display:none'
-  document.getElementById('therm6').style = 'display:none'
-  document.getElementById('none').style.display='none'
-  document.getElementById('one').style.display='none'
-  document.getElementById('two').style.display='block'
-  document.getElementById('four').style.display='none'
-  document.getElementById('six').style.display='none'
-  document.getElementById('twelve').style.display='none'
+  document.getElementById('therm1').style = 'display:none';
+  document.getElementById('therm2').style = 'display:none';
+  document.getElementById('therm3').style = 'display:block';
+  document.getElementById('therm4').style = 'display:none';
+  document.getElementById('therm5').style = 'display:none';
+  document.getElementById('therm6').style = 'display:none';
+  document.getElementById('none').style.display='none';
+  document.getElementById('one').style.display='none';
+  document.getElementById('two').style.display='block';
+  document.getElementById('four').style.display='none';
+  document.getElementById('six').style.display='none';
+  document.getElementById('twelve').style.display='none';
+  document.getElementById('black1').style.display='none';
+  document.getElementById('simonhappy').style.display='none';
+  document.getElementById('hbeach').style.display='none';
+  document.getElementById('black2').style.display='none';
+  document.getElementById('coral').style.display='none';
+  document.getElementById('black3').style.display='block';
+  document.getElementById('glacier').style.display='block';
+  document.getElementById('black4').style.display='none';
+  document.getElementById('sealevel').style.display='none';
+  document.body.style.backgroundColor= "#ACACAC" ;
+  document.getElementById('title4').style.color='black';
 
 }
 
 function but4(){
-  document.getElementById('therm1').style = 'display:none'
-  document.getElementById('therm2').style = 'display:none'
-  document.getElementById('therm3').style = 'display:none'
-  document.getElementById('therm4').style = 'display:block'
-  document.getElementById('therm5').style = 'display:none'
-  document.getElementById('therm6').style = 'display:none'
-  document.getElementById('none').style.display='none'
-  document.getElementById('one').style.display='none'
-  document.getElementById('two').style.display='none'
-  document.getElementById('four').style.display='block'
-  document.getElementById('six').style.display='none'
-  document.getElementById('twelve').style.display='none'
+  document.getElementById('therm1').style = 'display:none';
+  document.getElementById('therm2').style = 'display:none';
+  document.getElementById('therm3').style = 'display:none';
+  document.getElementById('therm4').style = 'display:block';
+  document.getElementById('therm5').style = 'display:none';
+  document.getElementById('therm6').style = 'display:none';
+  document.getElementById('none').style.display='none';
+  document.getElementById('one').style.display='none';
+  document.getElementById('two').style.display='none';
+  document.getElementById('four').style.display='block';
+  document.getElementById('six').style.display='none';
+  document.getElementById('twelve').style.display='none';
+  document.getElementById('black1').style.display='none';
+  document.getElementById('simonhappy').style.display='none';
+  document.getElementById('hbeach').style.display='none';
+  document.getElementById('black2').style.display='none';
+  document.getElementById('coral').style.display='none';
+  document.getElementById('black3').style.display='none';
+  document.getElementById('glacier').style.display='none';
+  document.getElementById('black4').style.display='block';
+  document.getElementById('sealevel').style.display='block';
+  document.body.style.backgroundColor= "#7D7D7D" ;
+  document.getElementById('title4').style.color='black';
+
 }
 
 function but5(){
-  document.getElementById('therm1').style = 'display:none'
-  document.getElementById('therm2').style = 'display:none'
-  document.getElementById('therm3').style = 'display:none'
-  document.getElementById('therm4').style = 'display:none'
-  document.getElementById('therm5').style = 'display:block'
-  document.getElementById('therm6').style = 'display:none'
-  document.getElementById('none').style.display='none'
-  document.getElementById('one').style.display='none'
-  document.getElementById('two').style.display='none'
-  document.getElementById('four').style.display='none'
-  document.getElementById('six').style.display='block'
-  document.getElementById('twelve').style.display='none'
+  document.getElementById('therm1').style = 'display:none';
+  document.getElementById('therm2').style = 'display:none';
+  document.getElementById('therm3').style = 'display:none';
+  document.getElementById('therm4').style = 'display:none';
+  document.getElementById('therm5').style = 'display:block';
+  document.getElementById('therm6').style = 'display:none';
+  document.getElementById('none').style.display='none';
+  document.getElementById('one').style.display='none';
+  document.getElementById('two').style.display='none';
+  document.getElementById('four').style.display='none';
+  document.getElementById('six').style.display='block';
+  document.getElementById('twelve').style.display='none';
+  document.getElementById('black1').style.display='none';
+  document.getElementById('simonhappy').style.display='none';
+  document.getElementById('hbeach').style.display='none';
+  document.getElementById('black2').style.display='none';
+  document.getElementById('black3').style.display='none';
+  document.getElementById('coral').style.display='none';
+  document.getElementById('glacier').style.display='none';
+  document.getElementById('black4').style.display='none';
+  document.getElementById('sealevel').style.display='none';
+  document.body.style.backgroundColor= "#4E4E4E" ;
+  document.getElementById('title4').style.color='white';
 }
 
 function but6(){
-  document.getElementById('therm1').style = 'display:none'
-  document.getElementById('therm2').style = 'display:none'
-  document.getElementById('therm3').style = 'display:none'
-  document.getElementById('therm4').style = 'display:none'
-  document.getElementById('therm5').style = 'display:none'
-  document.getElementById('therm6').style = 'display:block'
-  document.getElementById('none').style.display='none'
-  document.getElementById('one').style.display='none'
-  document.getElementById('two').style.display='none'
-  document.getElementById('four').style.display='none'
-  document.getElementById('six').style.display='none'
-  document.getElementById('twelve').style.display='block'
+  document.getElementById('therm1').style = 'display:none';
+  document.getElementById('therm2').style = 'display:none';
+  document.getElementById('therm3').style = 'display:none';
+  document.getElementById('therm4').style = 'display:none';
+  document.getElementById('therm5').style = 'display:none';
+  document.getElementById('therm6').style = 'display:block';
+  document.getElementById('none').style.display='none';
+  document.getElementById('one').style.display='none';
+  document.getElementById('two').style.display='none';
+  document.getElementById('four').style.display='none';
+  document.getElementById('six').style.display='none';
+  document.getElementById('twelve').style.display='block';
+  document.getElementById('black1').style.display='none';
+  document.getElementById('simonhappy').style.display='none';
+  document.getElementById('hbeach').style.display='none';
+  document.getElementById('black2').style.display='none';
+  document.getElementById('glacier').style.display='none';
+  document.getElementById('black3').style.display='none';
+  document.getElementById('coral').style.display='none';
+  document.getElementById('black4').style.display='none';
+  document.getElementById('sealevel').style.display='none';
+  document.body.style.backgroundColor= "#292929" ;
+  document.getElementById('title4').style.color='white';
+
 }
 
 function hide(){
