86c778eea60404d6bb702863518e2f5b07d81fdd
gwc <mgs0723@gmail.com>
2019-07-26 14:15:10 -0700

    plastic, about, biomes

--- more.js
+++ more.js
@@ -1,95 +1,95 @@
 function but1(){
-  document.getElementById('therm1').style = 'display:block'
-  document.getElementById('therm2').style = 'display:none'
-  document.getElementById('therm3').style = 'display:none'
-  document.getElementById('therm4').style = 'display:none'
-  document.getElementById('therm5').style = 'display:none'
-  document.getElementById('therm6').style = 'display:none'
-  document.getElementById('none').style.display='block'
-  document.getElementById('one').style.display='none'
-  document.getElementById('two').style.display='none'
-  document.getElementById('four').style.display='none'
-  document.getElementById('six').style.display='none'
-  document.getElementById('twelve').style.display='none'
+  document.getElementById('therm1').style = 'display:block';
+  document.getElementById('therm2').style = 'display:none';
+  document.getElementById('therm3').style = 'display:none';
+  document.getElementById('therm4').style = 'display:none';
+  document.getElementById('therm5').style = 'display:none';
+  document.getElementById('therm6').style = 'display:none';
+  document.getElementById('none').style.display='block';
+  document.getElementById('one').style.display='none';
+  document.getElementById('two').style.display='none';
+  document.getElementById('four').style.display='none';
+  document.getElementById('six').style.display='none';
+  document.getElementById('twelve').style.display='none';
 
 
 }
 
 function but2(){
-  document.getElementById('therm1').style = 'display:none'
-  document.getElementById('therm2').style = 'display:block'
-  document.getElementById('therm3').style = 'display:none'
-  document.getElementById('therm4').style = 'display:none'
-  document.getElementById('therm5').style = 'display:none'
-  document.getElementById('therm6').style = 'display:none'
-  document.getElementById('none').style.display='none'
-  document.getElementById('one').style.display='block'
-  document.getElementById('two').style.display='none'
-  document.getElementById('four').style.display='none'
-  document.getElementById('six').style.display='none'
-  document.getElementById('twelve').style.display='none'
+  document.getElementById('therm1').style = 'display:none';
+  document.getElementById('therm2').style = 'display:block';
+  document.getElementById('therm3').style = 'display:none';
+  document.getElementById('therm4').style = 'display:none';
+  document.getElementById('therm5').style = 'display:none';
+  document.getElementById('therm6').style = 'display:none';
+  document.getElementById('none').style.display='none';
+  document.getElementById('one').style.display='block';
+  document.getElementById('two').style.display='none';
+  document.getElementById('four').style.display='none';
+  document.getElementById('six').style.display='none';
+  document.getElementById('twelve').style.display='none';
 
 }
 
 function but3(){
-  document.getElementById('therm1').style = 'display:none'
-  document.getElementById('therm2').style = 'display:none'
-  document.getElementById('therm3').style = 'display:block'
-  document.getElementById('therm4').style = 'display:none'
-  document.getElementById('therm5').style = 'display:none'
-  document.getElementById('therm6').style = 'display:none'
-  document.getElementById('none').style.display='none'
-  document.getElementById('one').style.display='none'
-  document.getElementById('two').style.display='block'
-  document.getElementById('four').style.display='none'
-  document.getElementById('six').style.display='none'
-  document.getElementById('twelve').style.display='none'
+  document.getElementById('therm1').style = 'display:none';
+  document.getElementById('therm2').style = 'display:none';
+  document.getElementById('therm3').style = 'display:block';
+  document.getElementById('therm4').style = 'display:none';
+  document.getElementById('therm5').style = 'display:none';
+  document.getElementById('therm6').style = 'display:none';
+  document.getElementById('none').style.display='none';
+  document.getElementById('one').style.display='none';
+  document.getElementById('two').style.display='block';
+  document.getElementById('four').style.display='none';
+  document.getElementById('six').style.display='none';
+  document.getElementById('twelve').style.display='none';
 
 }
 
 function but4(){
-  document.getElementById('therm1').style = 'display:none'
-  document.getElementById('therm2').style = 'display:none'
-  document.getElementById('therm3').style = 'display:none'
-  document.getElementById('therm4').style = 'display:block'
-  document.getElementById('therm5').style = 'display:none'
-  document.getElementById('therm6').style = 'display:none'
-  document.getElementById('none').style.display='none'
-  document.getElementById('one').style.display='none'
-  document.getElementById('two').style.display='none'
-  document.getElementById('four').style.display='block'
-  document.getElementById('six').style.display='none'
-  document.getElementById('twelve').style.display='none'
+  document.getElementById('therm1').style = 'display:none';
+  document.getElementById('therm2').style = 'display:none';
+  document.getElementById('therm3').style = 'display:none';
+  document.getElementById('therm4').style = 'display:block';
+  document.getElementById('therm5').style = 'display:none';
+  document.getElementById('therm6').style = 'display:none';
+  document.getElementById('none').style.display='none';
+  document.getElementById('one').style.display='none';
+  document.getElementById('two').style.display='none';
+  document.getElementById('four').style.display='block';
+  document.getElementById('six').style.display='none';
+  document.getElementById('twelve').style.display='none';
 }
 
 function but5(){
-  document.getElementById('therm1').style = 'display:none'
-  document.getElementById('therm2').style = 'display:none'
-  document.getElementById('therm3').style = 'display:none'
-  document.getElementById('therm4').style = 'display:none'
-  document.getElementById('therm5').style = 'display:block'
-  document.getElementById('therm6').style = 'display:none'
-  document.getElementById('none').style.display='none'
-  document.getElementById('one').style.display='none'
-  document.getElementById('two').style.display='none'
-  document.getElementById('four').style.display='none'
-  document.getElementById('six').style.display='block'
-  document.getElementById('twelve').style.display='none'
+  document.getElementById('therm1').style = 'display:none';
+  document.getElementById('therm2').style = 'display:none';
+  document.getElementById('therm3').style = 'display:none';
+  document.getElementById('therm4').style = 'display:none';
+  document.getElementById('therm5').style = 'display:block';
+  document.getElementById('therm6').style = 'display:none';
+  document.getElementById('none').style.display='none';
+  document.getElementById('one').style.display='none';
+  document.getElementById('two').style.display='none';
+  document.getElementById('four').style.display='none';
+  document.getElementById('six').style.display='block';
+  document.getElementById('twelve').style.display='none';
 }
 
 function but6(){
-  document.getElementById('therm1').style = 'display:none'
-  document.getElementById('therm2').style = 'display:none'
-  document.getElementById('therm3').style = 'display:none'
-  document.getElementById('therm4').style = 'display:none'
-  document.getElementById('therm5').style = 'display:none'
-  document.getElementById('therm6').style = 'display:block'
-  document.getElementById('none').style.display='none'
-  document.getElementById('one').style.display='none'
-  document.getElementById('two').style.display='none'
-  document.getElementById('four').style.display='none'
-  document.getElementById('six').style.display='none'
-  document.getElementById('twelve').style.display='block'
+  document.getElementById('therm1').style = 'display:none';
+  document.getElementById('therm2').style = 'display:none';
+  document.getElementById('therm3').style = 'display:none';
+  document.getElementById('therm4').style = 'display:none';
+  document.getElementById('therm5').style = 'display:none';
+  document.getElementById('therm6').style = 'display:block';
+  document.getElementById('none').style.display='none';
+  document.getElementById('one').style.display='none';
+  document.getElementById('two').style.display='none';
+  document.getElementById('four').style.display='none';
+  document.getElementById('six').style.display='none';
+  document.getElementById('twelve').style.display='block';
 }
 
 function hide(){
